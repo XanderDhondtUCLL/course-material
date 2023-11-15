169a5739dc178e38549c6bcba3567a077b62ebcc
Xander D'hondt <xander.dhondt@student.ucll.be>
2023-11-15 19:28:01 +0100

    Nu maar pas introduction gedaan... oeps

--- exercises/file-io/student.py
+++ exercises/file-io/student.py
@@ -4,3 +4,19 @@ def count_lines_in_file(path):
         while file.readline():
             count += 1
     return count
+
+def remove_empty_lines(source, destination):
+    with open(source, encoding='utf-8') as file:
+        with open(destination, 'w', encoding='utf-8') as output:
+            for line in file:
+                if line != '\n':
+                    output.write(line)
+
+def remove_duplicate_lines(source, destination):
+    with open(source, encoding='utf-8') as file:
+        with open(destination, 'w', encoding='utf-8') as output:
+            previous_line = None
+            for line in file:
+                if line != previous_line:
+                    output.write(line)
+                    previous_line = line
