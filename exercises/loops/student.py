47757cf6f73f991b80536a8c13ba62ae0b074014
XanderDhondtUCLL <xander.dhondt@student.ucll.be>
2023-11-16 13:55:44 +0100

    Loops afgemaakt

--- exercises/loops/student.py
+++ exercises/loops/student.py
@@ -59,3 +59,28 @@ def is_prime(n):
         if n % i == 0:
             return False
     return n > 1
+
+def gcd(x, y):
+    for i in range(min(abs(x), abs(y)), 0, -1):
+        if x % i == 0 and y % i == 0:
+            return i
+        
+def valid_parentheses(string):
+    count = 0
+    for char in string:
+        if char == '(':
+            count += 1
+        elif char == ')':
+            count -= 1
+            if count < 0:
+                return False
+    return count == 0
+
+def remove_backspaces(string):
+    result = ''
+    for char in string:
+        if char == '\b':
+            result = result[:-1]
+        else:
+            result += char
+    return result
